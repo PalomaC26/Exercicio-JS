6d80f8ceb82d5e46e7032b14b9c80965c5c54696
PalomaC26 <carvalhodasilvapaloma133@gmail.com>
2024-05-27 10:26:36 -0300

    atividadejs

---
+++ script.js
@@ -0,0 +1,51 @@
+document.getElementById('mostrar-variaveis').addEventListener('click', function() {
+
+    // Variáveis Simples
+let idade = 25;
+let nome = "Ana";
+let casado = true;
+
+
+document.getElementById('variavel-simples').innerText = `Simples: Nome ${nome}, Idade: ${idade} Casado: ${casado}`;
+
+// Variáveis Compostas
+let frutas = ["maçã", "banana", "laranja"];
+let pessoa ={
+    nome: "Carlos",
+    idade: 30,
+    casado: false
+};
+ 
+document.getElementById('variavel-composta').innerText = `Compostas: Frutas: ${frutas.join(', ')}
+ 
+Pessoa: Nome ${pessoa.nome}, Idade: ${pessoa.idade}, Casado: ${pessoa.casado}`;
+
+// Variáveis de Variáveis
+let a = 10;
+let b = 20;
+let c = 30;
+
+let variáveis = [a, b, c];
+let chave = "dinamica";
+let objetoDinamico = {
+    dinamica: "Este é um valor dinamico"
+};
+
+
+document.getElementById('variavel-de-variaveis').innerText = `Variaveis de variaveis:
+
+Array: ${variaveis.join(', ')}, Objeto Dinamico: ${objetoDinamico[chave]}`;
+
+});
+
+// Exercicio: Adicionar Frutas ao Array
+let frutasArray = ["maçã", "banana", "laranja"];
+
+
+document.getElementById('form-frutas').addEventListener('submit', function(event){
+    event.preventDefault();
+    let novafruta = document.getElementById('nova-fruta').value;
+    frutasArray.push(novaFruta);
+    document.getElementById('lista-frutas').innerText = `Lista de Frutas: ${frutasArray.jpoin(', ')}`;
+    document.getElementById('nova-fruta').value = '';
+});
